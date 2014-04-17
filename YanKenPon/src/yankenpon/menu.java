b56dc4e46563dc87f1ef9c394e25a5dc3c26253a
KilnOfTheSecondFlame <pb@comfox.ch>
2014-04-17 15:12:55 +0200

    Fleshed out menu according to the structure chart

--- YanKenPon/src/yankenpon/menu.java
+++ YanKenPon/src/yankenpon/menu.java
@@ -6,20 +6,34 @@
 
 package yankenpon;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pascalbaumann
  */
 public class menu {
-
+    Scanner Puffer = new Scanner(System.in);
+    private String opponent;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        
-        //                 :)
+        menu m = new menu();
+        m.setOpponent();
+        System.out.print(m.getOpponent() + "\n");
     }
+
+    public String getOpponent() {
+        return opponent;
+    }
+
+    public void setOpponent() {
+        System.out.println("Choose your opponent: Randy, Ray, Robbie");
+        this.opponent=Puffer.nextLine();
+    }
+    
+    
     
 }
