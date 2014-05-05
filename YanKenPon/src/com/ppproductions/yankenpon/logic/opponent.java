cf88c1dfa6d84ded658c85f4042c2ff0b70babf7
KilnOfTheSecondFlame <pb@comfox.ch>
2014-05-05 18:36:39 +0200

    Merging

---
+++ YanKenPon/src/com/ppproductions/yankenpon/logic/opponent.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ppproductions.yankenpon.logic;
+
+/**
+ *
+ * @author pascalbaumann
+ */
+public class opponent {
+    private Float pRock;
+    private Float pScissors;
+    
+    public opponent(String Name){
+        
+    } 
+    
+    
+}
