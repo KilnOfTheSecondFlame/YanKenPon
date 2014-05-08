a68e39ecc397c0617b852827f71b8209b62e6764
KilnOfTheSecondFlame <pb@comfox.ch>
2014-05-08 14:52:43 +0200

    Testing

--- YanKenPon/src/com/ppproductions/yankenpon/logic/opponent.java
+++ YanKenPon/src/com/ppproductions/yankenpon/logic/opponent.java
@@ -60,6 +60,7 @@ public class opponent {
     public String getPon(){
         String pon;
         Double decidedMove=randomGenerator.nextDouble();
+        System.out.println(decidedMove);
         if (decidedMove<=pRock) pon="Rock";
         else if (decidedMove<=(pRock+pScissors)) pon="Scissors";
         else pon="Paper";
