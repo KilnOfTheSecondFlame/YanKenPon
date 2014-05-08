6c10311d35e2794071d108f2bb6acc886bce9d43
KilnOfTheSecondFlame <pb@comfox.ch>
2014-05-08 11:56:49 +0200

    Implemented Attack-Chooser Implemented different Opponents Implemented SpeciaAttack

--- YanKenPon/src/com/ppproductions/yankenpon/logic/opponent.java
+++ YanKenPon/src/com/ppproductions/yankenpon/logic/opponent.java
@@ -6,23 +6,49 @@
 
 package com.ppproductions.yankenpon.logic;
 
+import java.util.Random;
+
 /**
  *
  * @author pascalbaumann
  */
 public class opponent {
-    private Double pRock;
-    private Double pScissors;
-    private String Name;
+    final private Double pRock;
+    final private Double pScissors;
+    final private String Name;
+    final private Random randomGenerator = new Random();
+    
     
     public opponent(){
-        pRock=.3;
-        pScissors=.3;
+        pRock=Double.valueOf(1/3);
+        pScissors=Double.valueOf(1/3);
         Name="Default";
     }
     
     public opponent(String Name){
-        
+        this.Name=Name;
+        switch (Name) {
+            case "Rocky":
+                pRock=1.0;
+                pScissors=0.0;
+                break;
+            case "Dwayne 'The Rock' Johnson":
+                pRock=0.8;
+                pScissors=1.0;
+                break;
+            case "Edward":
+                pRock=0.0;
+                pScissors=1.0;
+                break;
+            case "Paperboy":
+                pRock=0.0;
+                pScissors=0.0;
+                break;
+            default:
+                pRock=Double.valueOf(1/3);
+                pScissors=Double.valueOf(1/3);
+                break;
+        }
     }
 
     public String getName() {
@@ -31,8 +57,23 @@ public class opponent {
     
     public String getPon(){
         String pon;
-        
+        Double decidedMove=randomGenerator.nextDouble();
+        if (decidedMove<=pRock) pon="Rock";
+        else if (decidedMove<=(pRock+pScissors)) pon="Scissors";
+        else pon="Paper";
         return pon;
     }
     
+    public String getPonSpecial(){
+        String pon;
+        switch (Name){
+            case "Saruman":
+                pon="Fireball";
+                break;
+            default:
+                pon=getPon();
+                break;
+        }
+        return pon;
+    }
 }
